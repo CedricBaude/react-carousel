b5235fd33e79780b2b6346f29144592e438db551
Cedric Baude <baude.cedric.perso@gmail.com>
2023-01-26 22:42:50 +0100

    carousel style add

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import { countries } from './components/Data';
 function App() {
 	return (
 		<div className="App">
-			<h2>Place to meet</h2>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+			<div className="topContent">
+				<h2>Place to meet</h2>
+				<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+			</div>
 			{/* Carousel */}
 			<Carousel images={countries} />
 		</div>
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -4,9 +4,18 @@ import '../style/carousel.css'
 const Carousel = ({ images }) => {
     return (
         <div className='carousel'>
-            {images.map((image) => {
-                return <div>{image.title}</div>
-            })}
+            <div className="carousel_wrapper">
+                {images.map((image, index) => {
+                    return (
+                        <div key={index} className='carousel_card'>
+                            <img className='card_image' src={image.image} alt="" />
+                            <div className="card_overlay">
+                                <h2 className="card_title">{image.title}</h2>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 };
