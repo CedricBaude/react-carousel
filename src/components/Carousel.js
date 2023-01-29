28d6d3992ba81632f06a8c97d0acb0cc32045c13
Cedric Baude <baude.cedric.perso@gmail.com>
2023-01-29 18:52:46 +0100

    add autoplay

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../style/carousel.css'
 
 const Carousel = ({ images }) => {
     const [current, setCurrent] = useState(0);
+    const [autoPlay, setAutoPlay] = useState(true);
+    let timeOut = null;
+
+    useEffect(() => {
+        timeOut = autoPlay && setTimeout(() => {
+            slideRight();
+        }, 2500);
+    });
 
     const slideRight = () => {
         setCurrent(current === images.length - 1 ? 0 : current + 1)
@@ -13,11 +21,11 @@ const Carousel = ({ images }) => {
     };
 
     return (
-        <div className='carousel'>
+        <div className='carousel' onMouseEnter={() => { setAutoPlay(false); clearTimeout(timeOut) }} onMouseLeave={() => { setAutoPlay(true) }}>
             <div className="carousel_wrapper">
                 {images.map((image, index) => {
                     return (
-                        <div key={index} className={index == current ? "carousel_card carousel_card-active" : "carousel_card"}>
+                        <div key={index} className={index === current ? "carousel_card carousel_card-active" : "carousel_card"}>
                             <img className='card_image' src={image.image} alt="" />
                             <div className="card_overlay">
                                 <h2 className="card_title">{image.title}</h2>
@@ -30,7 +38,7 @@ const Carousel = ({ images }) => {
                 <div className="carousel_pagination">
                     {images.map((_, index) => {
                         return (
-                            <div key={index} className={index == current ? "pagination_dot pagination_dot-active" : "pagination_dot"} onClick={() => setCurrent(index)}></div>
+                            <div key={index} className={index === current ? "pagination_dot pagination_dot-active" : "pagination_dot"} onClick={() => setCurrent(index)}></div>
                         )
                     })}
                 </div>
